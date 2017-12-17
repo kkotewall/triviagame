d742b65a7cc26c9bad78d60909c26f06425a94fc
Kayla <k.kotewall@gmail.com>
2017-12-17 14:04:10 -0600

    pseudocode and update play

---
+++ assets/javascript/app.js
@@ -0,0 +1,32 @@
+// variable list
+
+
+// array of trivia questions
+
+
+// array of correct answers: not displayed
+
+
+// array of displayed options
+
+
+// wins counter
+
+
+// loss counter
+
+
+// unanswered counter
+
+
+// game timer: displayed
+
+
+// interval timer: not displayed
+
+
+// start game button
+
+
+// restart game button
+
