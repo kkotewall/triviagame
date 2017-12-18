7d95d26b2eb9d05501531249a7112ea53f5f94ef
Kayla <k.kotewall@gmail.com>
2017-12-18 01:03:33 -0600

    trivia object and user points function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,25 +1,55 @@
 // variable list
 
 
-// array of trivia questions
+// trivia object with question arrays
+var trivia = [ {
+    "question":"question 1?",
+    "options":["A","B","C","D"],
+    "answer":"B"
+  }, {
+    "question":"question 2?",
+    "options":["A","B","C","D"],
+    "answer":"B"
+  }, {
+    "question":"question 3?",
+    "options":["A","B","C","D"],
+    "answer":"B"
+  	} 
+  ];
 
 
-// array of correct answers: not displayed
 
-
-// array of displayed options
-
-
+function userGuess(result) {
 // wins counter
-
+	// question: how to cause correct.html to load
+  if (guess === answer) {
+    wins++;
+    $("#win-lose").html("<h2>Winner!</h2>");
+  }
 
 // loss counter
-
+    // question: how to cause incorrect.html to load
+  else if (guess !== answer) {
+    losses++;
+    $("#win-lose").html("<h2>Loser!</h2>");
+  }
 
 // unanswered counter
+	//question: how to load time.html
+else (guess === null) {
+    losses++;
+    $("#win-lose").html("<h2>Time's Up!</h2>");
+  }
+};
+
+
+// timer settings
+setTimemer(threeSeconds, 1000 * 3);
+setTimemer(fifteenSeconds, 1000 * 15);
 
 
 // game timer: displayed
+
 
 
 // interval timer: not displayed
