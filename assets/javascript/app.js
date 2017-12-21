0b5e1c9c9f5d378423d8f317c0e35a19b9aee1e2
Kayla <k.kotewall@gmail.com>
2017-12-21 16:36:48 -0600

    trivia questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,19 +1,53 @@
 // variable list
 
+//Create a variable to hold boolean value. Did the user select the right answer (true or false)?
+var selectRightAnswer = false;
 
-// trivia object with question arrays
-var trivia = [ {
-    "question":"question 1?",
-    "options":["A","B","C","D"],
-    "answer":"B"
-  }, {
-    "question":"question 2?",
-    "options":["A","B","C","D"],
-    "answer":"B"
-  }, {
-    "question":"question 3?",
-    "options":["A","B","C","D"],
-    "answer":"B"
+//Create variables to hold number of correct answers and incorrect answers. Display numbers when game is over.
+var correctAnswersTally = 0;
+var incorrectAnswersTally = 0;
+
+//Create variable to hold count that will keep track of the index of the currently displayed trivia question.
+var count = 0;
+
+//Create variable to hold the time to answer a question.
+var Timer = 7;
+var intervalId;
+
+//Create variable to hold the user's choice/anser
+var userChoice;
+
+//Create variable for button so that we can create it using jQuery.
+var choiceBtn;
+
+
+// trivia object with question array
+var triviaDeck = {
+	triviaArray: [{
+    	question:"The modern viola was designed in what country?",
+    	options:["Frace","Germnay","Italy","Romania"],
+    	answer:"Italy"
+  		}, {
+  		question:"What clef is viola music primary written?",
+    	options:["Bass","Contra-alto","Alto","Treble"],
+    	answer:"Alto"
+  		}, {
+  		question:"Prior to the 20th century, composers preferred violists perform what part of an ensemble?",
+    	options:["solo","melody","counter-melody","harmony"],
+    	answer:"harmony"
+  		}, {
+  		question:"What famous guitarist first learned to play music on the viola?",
+    	options:["B.B. King","Jimi Hendrix","Eric Clapton","Carlos Santana"],
+    	answer:"Jimi Hendrix"
+  		}, {
+  		question:"What famous Beatles’ song features the viola?",
+    	options:["Eleanor Rigby","Yesterday","Strawberry Fields Forever","Love Me Do"],
+    	answer:"Eleanor Rigby"
+  		}, {
+  		question:"Which modern musician plays an electric viola?",
+    	options:["Papa John Creach (Jefferson Airplane)","Amy Lee (Evanescence)","Frank Zappa","John Cole (The Velvet Underground)"],
+    	answer:"John Cole (The Velvet Underground)"
+  		}, {
   	} 
   ];
 
